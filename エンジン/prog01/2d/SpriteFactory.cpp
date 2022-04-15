824d1acdf2f511aa41f56a8433d2ad2dcba0b3ef
tani <k020g1507@g.neec.ac.jp>
2022-04-16 01:39:49 +0900

    SpriteFactory追加
    ロードは最初だけに

---
+++ エンジン/prog01/2d/SpriteFactory.cpp
@@ -0,0 +1,37 @@
+#include "SpriteFactory.h"
+
+void SpriteFactory::Initialize()
+{
+	// デバッグテキスト用テクスチャ読み込み
+	if (!Sprite::LoadTexture(0, L"Resources/Sprite/debugfont.png"))
+	{
+		assert(0);
+	}
+	// デバッグテキスト初期化
+	DebugText::GetInstance()->Initialize(0);
+
+	// テクスチャ読み込み
+	if (!Sprite::LoadTexture(1, L"Resources/Sprite/APEX_01.png"))
+	{
+		assert(0);
+	}
+
+	// テクスチャ読み込み
+	if (!Sprite::LoadTexture(2, L"Resources/Sprite/weve1.png")) {
+		assert(0);
+	}
+	// テクスチャ読み込み
+	if (!Sprite::LoadTexture(3, L"Resources/Sprite/weve2.png")) {
+		assert(0);
+	}
+	// テクスチャ読み込み
+	if (!Sprite::LoadTexture(4, L"Resources/Sprite/weve3.png")) {
+		assert(0);
+	}
+}
+
+SpriteFactory* SpriteFactory::GetInstance()
+{
+	static SpriteFactory instance;
+	return &instance;
+}
